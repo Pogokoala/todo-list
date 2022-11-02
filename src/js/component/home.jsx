7b71947875ea04ecb9af6e8776fe3eb64a184a07
Pogokoala <pogo_33125@yahoo.com>
2022-11-02 09:22:15 +0000

    half way done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="todoListBox">
+				<header>
+					<h1>To Do List</h1>
+				</header>
+				<Form
+					setInputText={setInputText}
+					inputText={inputText}
+					setTodos={setTodos}
+					todos={todos}
+				/>
+				<div className="todoAI">
+					<TodoList setTodos={setTodos} todos={todos} />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const Form = ({ inputText, setInputText, todos, setTodos }) => {
+	const todoHandler = (e) => {
+		console.log(e.target.value);
+		setInputText(e.target.value);
+	};
+	const submitHandler = (e) => {
+		e.preventDefault();
+		setTodos([
+		  ...todos,
+		  { text: inputText, completed: false, id: Math.random() * 1000 },
+		]);
+		setInputText("");
+	  };
+	  return (
+		<form>
+		  <input
+			onChange={todoHandler}
+			value={inputText}
+			type="text"
+			className="todoInput"
+		  />
+		  <button onClick={submitHandler} className="addButton" type="submit">
+			<span>add</span>
+		  </button>
+		</form>
+	);
+};
+
+const TodoList = ({ todos, setTodos }) => {
+	return (
+		<div className="todoBox">
+			<ul className="todoUl">
+				{todos.map((todo) => (
+					<Todo
+						setTodos={setTodos}
+						todos={todos}
+						text={todo.text}
+						id={todo.id}
+						key={todo.id}
+									todo={todo}
+					/>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+const Todo = ({ text, todo, todos, setTodos }) => {
+	const deleteTodo = () => {
+		setTodos(todos.filter((e) => e.id !== todo.id));
+	};
+	return (
+		<div className="todo-list">
+			<li className="todo-item">{text}</li>
+			<button onClick={deleteTodo} className="remove-btn">Done</button>
 		</div>
 	);
 };
